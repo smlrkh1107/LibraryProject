91685a914f4d0edd92714a57d8a7431fc2782bb2
smlrkh1107 <krae7789230@gmail.com>
2020-04-21 21:44:56 +0900

    프로필 이미지 웹에서 긁어오기. =>: 아직이미지가 안뜨는게 맞아. 권한에 막혀 안떠. log보면 security머시기 뜸..

--- app/src/main/java/kun/hee/libraryproject/MainActivity.kt
+++ app/src/main/java/kun/hee/libraryproject/MainActivity.kt
@@ -2,6 +2,8 @@ package kun.hee.libraryproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +15,11 @@ class MainActivity : BaseActivity() {
 
     }
 
-    override fun setValues() {
+    override fun setValues() { //http로 시작하는 거 가져와보자
+        Glide.with(mContext).load("https://lh3.googleusercontent.com/proxy/nqdykTCq0qPvX7kSoNkvbPUb2QAjhBcQcYkPSByDvZV8ivoKXgfT8uAIvsiJpukSN5lAdIk6jRWNZcoP-TgW9SyeKrPO5m2OwfPcwIUHMzeKtXlnCw").into(profileImg)
+
+        //resourceId로 하면 R.ImageView. 딩가딩가 고
+
     }
 
 
